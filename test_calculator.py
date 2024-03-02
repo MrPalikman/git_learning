57f37f363774107ff02b311ec60812989faa53cb
MrPalikman <130242802+MrPalikman@users.noreply.github.com>
2024-03-02 14:09:26 +0300

    ТЗ

--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,10 @@
 from calculator import Calculator
 
+# Задание
+# 1. Разработчику 1 - реализовать функцию умножения
+# 2. Разработчику 2 - рефлизовать функцию деления
+
+
 calc = Calculator()
 calc.sum(5,5)
 print(calc.last_rez)
