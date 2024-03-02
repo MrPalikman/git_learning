1339549c386970cf1171e8c63fc0c9dccb8e56e0
MrPalikman <130242802+MrPalikman@users.noreply.github.com>
2024-03-02 14:59:09 +0300

    Task1 - реализовано умножение

--- calculator.py
+++ calculator.py
@@ -6,5 +6,8 @@ class Calculator:
         self.last_rez = n1 +n2
         return n1 + n2
 
+    def myltiply(self, n1, n2):
+        self.last_rez = n1 * n2
+        return self.last_rez
     def print_last_rez(self):
         print(self.last_rez)
