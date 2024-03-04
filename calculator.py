35de4c709f840967d141876ffec72da318014eb9
MrPalikman <130242802+MrPalikman@users.noreply.github.com>
2024-03-04 14:59:50 +0300

    task2 - функция деления

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import traceback
+
 
 class Calculator:
     last_rez = None
@@ -5,6 +7,14 @@ class Calculator:
     def sum(self, n1, n2):
         self.last_rez = n1 +n2
         return n1 + n2
+
+    def divide (self, n1, n2):
+        try:
+            res = n1 / n2
+            self.last_rez = res
+            return res
+        except:
+            traceback.print_exc()
 
     def print_last_rez(self):
         print(self.last_rez)
